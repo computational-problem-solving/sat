3c6ea919ce18ac9857ef575fae37623a9763b279
Aurélien Ooms <aurelien.ooms@gmail.com>
2017-03-01 05:13:39 -0500

    fix

--- src/core/index.js
+++ src/core/index.js
@@ -2,4 +2,4 @@ export * from './SAT0W' ;
 export * from './_verify' ;
 export * from './convert' ;
 export * from './parse' ;
-export * from './setup_assigment' ;
+export * from './setup_assignment' ;
