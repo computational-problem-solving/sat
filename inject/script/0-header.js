429b6eaec56ff57fac6805780eee5fc99a75d054
aureooms <aureooms@users.noreply.github.com>
2021-04-14 19:35:48 +0000

    Deploying to gh-pages from @ aureooms/js-sat@9abad7451cdf64778dcd939117f483a433321a0b 🚀

---
+++ inject/script/0-header.js
@@ -0,0 +1,30 @@
+const domReady = function (callback) {
+	const state = document.readyState;
+	if (state === 'interactive' || state === 'complete') {
+		callback();
+	} else {
+		document.addEventListener('DOMContentLoaded', callback);
+	}
+};
+
+domReady(() => {
+	const projectname = document.createElement('a');
+	projectname.classList.add('project-name');
+	projectname.text = 'aureooms/js-sat';
+	projectname.href = './index.html';
+
+	const header = document.querySelector('header');
+	header.insertBefore(projectname, header.firstChild);
+
+	const testlink = document.querySelector('header > a[data-ice="testLink"]');
+	testlink.href = 'https://coveralls.io/github/aureooms/js-sat';
+	testlink.target = '_BLANK';
+
+	const searchBox = document.querySelector('.search-box');
+	const input = document.querySelector('.search-input');
+
+	// Active search box when focus on searchBox.
+	input.addEventListener('focus', () => {
+		searchBox.classList.add('active');
+	});
+});
